78d36822d40a3ee81284334a84e25fa007a465e3
Simon <simon.wilhelmstatter@gmail.com>
2020-03-19 14:37:17 +0100

    added definitions

--- max7219_driver/max7219_driver.c
+++ max7219_driver/max7219_driver.c
@@ -1,3 +1,37 @@
+#include "max7219_driver.h"
+#include <avr/io.h>
 
-#include max7219_driver.h
+void sendByte(uint8_t data) {
+    USIDR = data;
+    asm volatile (
+    "ldi r16,(1<<USIWM0)|(0<<USICS0)|(1<<USITC)" "\n\t"
+    "ldi r17,(1<<USIWM0)|(0<<USICS0)|(1<<USITC)|(1<<USICLK)" "\n\t"
+    "out USICR,r16" "\n\t"
+    "out USICR,r17" "\n\t"
+    "out USICR,r16" "\n\t"
+    "out USICR,r17" "\n\t"
+    "out USICR,r16" "\n\t"
+    "out USICR,r17" "\n\t"
+    "out USICR,r16" "\n\t"
+    "out USICR,r17" "\n\t"
+    "out USICR,r16" "\n\t"
+    "out USICR,r17" "\n\t"
+    "out USICR,r16" "\n\t"
+    "out USICR,r17" "\n\t"
+    "out USICR,r16" "\n\t"
+    "out USICR,r17" "\n\t"
+    "out USICR,r16" "\n\t"
+    "out USICR,r17" "\n\t"
+    );
+}
+
+void sendWord(uint16_t data) {
+    uint8_t lowerByte = data & 0xFF;
+    sendByte(data >> 8);
+    sendByte(lowerByte);
+}
+
+uint8_t receiveByte(void) {
+    return USIBR;
+}
 
--- max7219_driver/max7219_driver.h
+++ max7219_driver/max7219_driver.h
@@ -2,5 +2,12 @@
 #ifndef MAX7219_DRIVER_H
 #define MAX7219_DRIVER_H
 
+#include <inttypes.h>
+
+void sendByte(uint8_t data);
+void sendWord(uint16_t data);
+
+uint8_t receiveByte(void);
+
 
 #endif
