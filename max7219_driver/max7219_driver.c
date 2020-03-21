d110d914b0ada1777937d8b26de232d8f7f8f004
Simon <simon.wilhelmstatter@gmail.com>
2020-03-21 13:37:06 +0100

    added makros and send_command, worked

--- max7219_driver/max7219_driver.c
+++ max7219_driver/max7219_driver.c
@@ -1,26 +1,26 @@
-#include "max7219_driver.h"
+#include "MAX7219_driver.h"
 #include <avr/io.h>
 
-void driver_init() {
-    DDRB |= (1 << SCK) | (1 << DO) | (1 << CS);
-    PORTB |= (1 << CS);
-    USISR = 0;
-}
-
-void sendByte(uint8_t data) {
+static void send_byte(uint8_t data) {
     USIDR = data;
-    PORTB &= ~(1 << CS);
-
-    while (!(USISR & (1 << USICNT3))) {
-        USICR = (1 << USITC);
-        USICR = (1 << USITC) | (1 << USICLK);
+    //TODO: implement the following lines in assembly
+    while (!(USISR & (1 << USICNT3))) { //waits for 8 cycles
+        USICR = (1 << USIWM0) | (1 << USITC); //USIWM0 == 1 to use the 3-wire-mode
+        USICR = (1 << USIWM0) | (1 << USITC) | (1 << USICLK);
     }
     USISR = 0;
-    PORTB |= (1 << CS);
 }
 
-void sendWord(uint16_t data) {
-    uint8_t lowerByte = data & 0xFF;
-    sendByte(data >> 8);
-    sendByte(lowerByte);
+void max7219_init() {
+    DDRB |= (1 << MAX7219_SCK) | (1 << MAX7219_DO) | (1 << MAX7219_CS);
+    PORTB |= (1 << MAX7219_CS);
+    PORTB &= ~(1 << MAX7219_SCK);
+    USISR = 0;
 }
+
+void max7219_send_command(uint8_t address, uint8_t data) {
+    PORTB &= ~(1 << MAX7219_CS);
+    send_byte(address);
+    send_byte(data);
+    PORTB |= (1 << MAX7219_CS);
+} 
--- max7219_driver/max7219_driver.h
+++ max7219_driver/max7219_driver.h
@@ -4,13 +4,43 @@
 
 #include <inttypes.h>
 
-#define SCK PB2
-#define DO PB1
-#define CS PB0
+//pins
+#define MAX7219_SCK PB2
+#define MAX7219_DO PB1
+#define MAX7219_CS PB0
 
-void driver_init(void);
+//register address map
+#define MAX7219_NO_OP           0x0
+#define MAX7219_DIGIT_0         0x1
+#define MAX7219_DIGIT_1         0x2
+#define MAX7219_DIGIT_2         0x3
+#define MAX7219_DIGIT_3         0x4
+#define MAX7219_DIGIT_4         0x5
+#define MAX7219_DIGIT_5         0x6
+#define MAX7219_DIGIT_6         0x7
+#define MAX7219_DIGIT_7         0x8
+#define MAX7219_DECODE_MODE     0x9
+#define MAX7219_INTENSITY       0xA
+#define MAX7219_SCAN_LIMIT      0xB
+#define MAX7219_SHUTDOWN        0xC
+#define MAX7219_DISPLAY_TEST    0xF
 
-void sendByte(uint8_t data);
-void sendWord(uint16_t data);
+//register data
+#define MAX7219_SCAN_LIMIT_0    0x0
+#define MAX7219_SCAN_LIMIT_1    0x1
+#define MAX7219_SCAN_LIMIT_2    0x2
+#define MAX7219_SCAN_LIMIT_3    0x3
+#define MAX7219_SCAN_LIMIT_4    0x4
+#define MAX7219_SCAN_LIMIT_5    0x5
+#define MAX7219_SCAN_LIMIT_6    0x6
+#define MAX7219_SCAN_LIMIT_7    0x7
+#define MAX7219_SHUTDOWN_ON     0x0
+#define MAX7219_SHUTDOWN_OFF    0x1
+#define MAX7219_DISPLAY_TEST_ON 0x1
+#define MAX7219_DISPLAY_TEST_OFF 0x0
+
+//functions
+void max7219_init(void);
+void max7219_send_command(uint8_t address,uint8_t data);
 
 #endif
