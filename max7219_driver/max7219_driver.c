0978d407da12aac7752d6f62b9045e5bfe748e8c
Simon <simon.wilhelmstatter@gmail.com>
2020-03-25 16:09:48 +0100

    added display configuration to init()

--- max7219_driver/max7219_driver.c
+++ max7219_driver/max7219_driver.c
@@ -17,6 +17,10 @@ void max7219_init() {
     PORTB |= (1 << MAX7219_CS);
     PORTB &= ~(1 << MAX7219_SCK);
     USISR = 0;
+    max7219_send_command(MAX7219_SCAN_LIMIT, MAX7219_SCAN_LIMIT_7);
+    max7219_send_command(MAX7219_DISPLAY_TEST, MAX7219_DISPLAY_TEST_OFF);
+    max7219_send_command(MAX7219_SHUTDOWN, MAX7219_SHUTDOWN_OFF);
+    max7219_send_command(MAX7219_INTENSITY, MAX7219_INTENSITY_MID);
 }
 
 void max7219_send_command(uint8_t address, uint8_t data) {
