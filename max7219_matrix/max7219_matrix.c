ec02087be5638c1272fec5426ad1a5ed2736c621
Team 21, Simon <simon.wilhelmstaetter@uni-ulm.de>
2021-03-09 13:55:41 +0100

    fixed bug in max7219_matrix_set_col() + added conformity to the clang-warnings on the use of a signed integer operand with a binary bitwise operator

--- max7219_matrix/max7219_matrix.c
+++ max7219_matrix/max7219_matrix.c
@@ -4,19 +4,19 @@
 static uint8_t matrix[8];
 
 inline void max7219_matrix_set_pixel(uint8_t row, uint8_t col) {
-    matrix[row] |= (1 << (7 - col));
+    matrix[row] |= (1u << (uint8_t) (7 - col));
 }
 
 inline void max7219_matrix_set_row(uint8_t row, uint8_t data) {
     matrix[row] = data;
 }
 
-//MSB of data will be in col 0, LSB of data will be in col 7
+//MSB of data will be in row 0, LSB of data will be in row 7
 inline void max7219_matrix_set_col(uint8_t col, uint8_t data) {
-    uint8_t mask = ~(1 << (7 - col));
+    uint8_t mask = ~(1u << (uint8_t) (7 - col));
     for (uint8_t i = 0; i < 8; i++) {
-        matrix[i] &= mask; //set the column to 0
-        matrix[i] |= (data >> (7 - i)) & 1; //set the data
+        matrix[i] &= mask; //set the column col in row i to 0
+        matrix[i] |= ((uint8_t) ((uint8_t) data >> (uint8_t) (7 - i)) & 1u) << (uint8_t) (7 - col); //set the data
     }
 }
 
@@ -28,31 +28,31 @@ inline void max7219_matrix_set_matrix(const uint8_t data[8]) {
 
 inline void max7219_matrix_set_hex_string(uint64_t data) {
     for (uint8_t i = 0; i < 8; i++) {
-        matrix[i] = 0xFF & (data >> (8 * (7 - i)));
+        matrix[i] = 0xFFu & (data >> (uint8_t) (8 * (7 - i)));
     }
 }
 
 inline void max7219_matrix_shift_left(void) {
     for (uint8_t i = 0; i < 8; i++) {
-        matrix[i] <<= 1;
+        matrix[i] <<= 1u;
     }
 }
 
 inline void max7219_matrix_shift_right(void) {
     for (uint8_t i = 0; i < 8; i++) {
-        matrix[i] >>= 1;
+        matrix[i] >>= 1u;
     }
 }
 
 inline void max7219_matrix_shift_left_circular(void) {
     for (uint8_t i = 0; i < 8; i++) {
-        matrix[i] = (matrix[i] << 1) | (matrix[i] >> 7);
+        matrix[i] = (uint8_t) (matrix[i] << 1u) | (uint8_t) (matrix[i] >> 7u);
     }
 }
 
 inline void max7219_matrix_shift_right_circular(void) {
     for (uint8_t i = 0; i < 8; i++) {
-        matrix[i] = (matrix[i] << 7) | (matrix[i] >> 1);
+        matrix[i] = (uint8_t) (matrix[i] << 7u) | (uint8_t) (matrix[i] >> 1u);
     }
 }
 
@@ -66,7 +66,7 @@ inline void max7219_matrix_rotate_left(void) {
     uint8_t temp[8] = {0};
     for (uint8_t i = 0; i < 8; i++) {
         for (uint8_t j = 0; j < 8; j++) {
-            temp[j] |= ((matrix[i] & (1 << j)) >> j) << (7-i);
+            temp[j] |= (uint8_t) ((uint8_t) ((uint8_t) (matrix[i] & (uint8_t) (1u << j)) >> j) << (uint8_t) (7 - i));
         }
     }
     for (uint8_t i = 0; i < 8; i++) {
@@ -78,7 +78,7 @@ inline void max7219_matrix_rotate_right(void) {
     uint8_t temp[8] = {0};
     for (uint8_t i = 0; i < 8; i++) {
         for (uint8_t j = 0; j < 8; j++) {
-            temp[7-j] |= ((matrix[i] & (1 << j)) >> j) << i;
+            temp[7-j] |= (uint8_t) ((uint8_t) ((uint8_t) (matrix[i] & (uint8_t) (1u << j)) >> j) << i);
         }
     }
     for (uint8_t i = 0; i < 8; i++) {
@@ -100,9 +100,9 @@ inline void max7219_matrix_flip_vertical(void) {
     for (uint8_t i = 0; i < 7; i++) {
         b = matrix[i];
         //reverse a byte
-        b = (b & 0xF0) >> 4 | (b & 0x0F) << 4;
-        b = (b & 0xCC) >> 2 | (b & 0x33) << 2;
-        b = (b & 0xAA) >> 1 | (b & 0x55) << 1;
+        b = (b & 0xF0u) >> 4u | (b & 0x0Fu) << 4u;
+        b = (b & 0xCCu) >> 2u | (b & 0x33u) << 2u;
+        b = (b & 0xAAu) >> 1u | (b & 0x55u) << 1u;
         matrix[i] = b;
     }
 }
